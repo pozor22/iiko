3a12d485bf53add6481882e51adc1e98cc19602b
Pozor <shaginov1b@yandex.ru>
2025-03-14 21:55:15 +0300

    added new viewsets for choice response serializer

---
+++ backend_iiko/backend/backend/viewsets.py
@@ -0,0 +1,30 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+
+class MyModelViewSet(ModelViewSet):
+    def response_serializer(self, *args, **kwargs):
+        return self.serializer_class(*args, **kwargs).data
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(self.response_serializer(serializer.instance), status=status.HTTP_201_CREATED, headers=headers)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(self.response_serializer(serializer.instance))
+
